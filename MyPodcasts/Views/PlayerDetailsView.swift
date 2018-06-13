20170fe27b73907a0fdaebacc1f57992819c8193
Richard Price <mrspondooli@gmail.com>
2018-06-13 18:23:49 +0100

    finished UIDesign using stack views

--- MyPodcasts/Views/PlayerDetailsView.swift
+++ MyPodcasts/Views/PlayerDetailsView.swift
@@ -18,20 +18,16 @@ class PlayerDetailsView: UIView {
     }
     @IBOutlet weak var episodeImageView: UIImageView!
     @IBOutlet weak var episodeTitle: UILabel!
+    @IBOutlet weak var episodeAuthor: UILabel!
+    @IBOutlet weak var episodePlayPauseButton: UIButton!
     
     var episode: Episode! {
-        
         didSet {
-            
             guard let url = URL(string: episode.imageUrl ?? "") else {return}
             episodeImageView.sd_setImage(with: url, completed: nil)
             episodeTitle.text = episode.title
-            
+            episodeAuthor.text = episode.author
+            print("\(episodeAuthor.text = episode.author)")
         }
-        
     }
-    
-    
-    
-    
 }
