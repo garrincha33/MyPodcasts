b864f44caacd3a9aedb9364aa0c277ca815f1f22
Richard Price <mrspondooli@gmail.com>
2018-06-29 08:06:59 +0100

    created custom FavouriteCell, added coded stackview

--- MyPodcasts/Controllers/FavouritesController.swift
+++ MyPodcasts/Controllers/FavouritesController.swift
@@ -20,8 +20,8 @@ class FavouritesController: UICollectionViewController, UICollectionViewDelegate
     }
     
     fileprivate func setupCollectionView() {
-        collectionView?.backgroundColor = .blue
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.backgroundColor = .white
+        collectionView?.register(FavouritePodcastCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     //MARK:- Collection View Delegate / Spacing methods
@@ -31,31 +31,25 @@ class FavouritesController: UICollectionViewController, UICollectionViewDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
         return cell
-    
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         let width = (view.frame.width - 3 * 16) / 2
-        
-        return CGSize(width: width, height: width)
-        
+        return CGSize(width: width, height: width + 46)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
         return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-        
+
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         
         return 16
-        
         
     }
 }
---
+++ MyPodcasts/Views/FavouritePodcastCell.swift
@@ -0,0 +1,48 @@
+//
+//  FavouritePodcastCell.swift
+//  MyPodcasts
+//
+//  Created by Richard Price on 29/06/2018.
+//  Copyright © 2018 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class FavouritePodcastCell: UICollectionViewCell {
+    
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "appicon"))
+    let nameLable = UILabel()
+    let artistNameLable = UILabel()
+    
+    fileprivate func stylizeUI() {
+        nameLable.text = "Podcast Name"
+        nameLable.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        artistNameLable.text = "Artist Name"
+        artistNameLable.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        artistNameLable.textColor = .lightGray
+    }
+    
+    fileprivate func setupViews() {
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        let stackView = UIStackView(arrangedSubviews: [imageView, nameLable, artistNameLable])
+        stackView.axis = .vertical
+        //enables autolayout
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        stylizeUI()
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
